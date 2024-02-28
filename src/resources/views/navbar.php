54da4f64b51b4ad2330493effe3412bb1bec6569
Misaraka <parametgm5@gmail.com>
2024-02-28 20:21:12 +0700

    add img

---
+++ src/resources/views/navbar.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<style>
+    .website_name {
+            font-family: 'Maitree', sans-serif;
+            font-weight: bold;
+    }
+</style>
+<nav class="navbar bg-secondary-content flex justify-between">
+        <div class="">
+            <img src="LearnsiteLogo.jpg" alt="" class="size-11 rounded-full  ml-2">
+            <h1 id="" class="website_name text-3xl  ml-2">LearnSite</h1>
+        </div>
+        <div class="">
+            <a class="btn btn-ghost text-xl">Course</a>
+            <a class="btn btn-ghost text-xl">Register</a>
+            <a class="btn btn-ghost text-xl">Login</a>
+        </div>
+    </nav>
