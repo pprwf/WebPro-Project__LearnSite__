82b420703cd5688b857ed2d574da52356b26ccbb
Earth_lojung <earth247god@gmail.com>
2024-02-29 18:15:18 +0700

    add quiz

--- src/resources/views/quiz.blade.php
+++ src/resources/views/quiz.blade.php
@@ -7,14 +7,44 @@
     <title>LearnSite</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.7.2/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {
+            background-color: #E4FFF2;
+        }
+
+        .color-1 {
+            background-color: #F1F1F1;
+        }
+
+        .centered-div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
 </head>
 
-<body class="bg-primary">
-    <div class="flex items-center justify-center h-screen">
-        <div class="content-center">
-            <div class="border-4 border-neutral m-10">
-                <p class="text-5xl">Quiz : 01 How to manage money</p>
-
+<body>
+    <div class="grid items-center justify-center h-screen">
+        <div class="min-h-screen flex items-center justify-center">
+            <div class="border-4 border-neutral m-10 roundded-lg">
+                <p class="text-5xl bg-emerald-500 text-white p-4 rounded-t-lg">Quiz : 01 How to manage money</p>
+                <div class="bg-white p-4 rounded-b-lg">
+                    <div class="grid items-center color-1">
+                        <p class="ml-5 text-xl">the quiz make for</p>
+                    </div>
+                    <p class="ml-5 mt-5 text-xl">Your Email : nanachi@gmail.com</p>
+                    <div class="border-2 border-neutral m-10 rounded-lg">
+                        <p class="text-3xl m-5">How Elon Musk walk be like him and you will rich</p>
+                        <div class="flex flex-col ml-5 mt-5 mb-5">
+                            <label><input type="radio" name="ans" value="A"> With legs</label>
+                            <label><input type="radio" name="ans" value="B"> With arms</label>
+                            <label><input type="radio" name="ans" value="C"> With both</label>
+                            <label><input type="radio" name="ans" value="D"> Without</label>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
